b5aaa13e734284b7356be1c29ac8eb0100f6968c
Anand Sasidharan Kartha <anandskartha@Mac.attlocal.net>
2024-12-11 11:23:58 -0600

    TodoList: fixing checkboxs

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import './TodoList.css';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -11,6 +12,9 @@ const TodoList = () => {
         const response = await fetch(
           'https://jsonplaceholder.typicode.com/todos/',
         );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const data = await response.json();
         setTodos(data);
       } catch (err) {
@@ -21,6 +25,14 @@ const TodoList = () => {
     };
     fetchData();
   }, []);
+
+  const handleCheck = (e, todoId) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  };
 
   if (loading) {
     return <p>Loading...</p>;
@@ -35,8 +47,12 @@ const TodoList = () => {
       <h1>Todo List</h1>
       <ul>
         {todos.map((todo) => (
-          <li className="todo-li" key={todo.id}>
-            <input type="checkbox" checked={todo.completed ?? false} />
+          <li className="todo-li no-list" key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed ?? false}
+              onChange={(e) => handleCheck(e, todo.id)}
+            />
             <span>{todo.title}</span>
           </li>
         ))}
